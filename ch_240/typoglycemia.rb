22e6d91ad0a30853dd1bb892e979ca77c38a100c
Alexey Ershov <alex4rom@gmail.com>
2015-12-03 19:09:55 +0300

    Add challange

--- ch_240/typoglycemia.rb
+++ ch_240/typoglycemia.rb
@@ -1,3 +1,16 @@
+# TODO: refactor :)
 def scramble(input)
-  words_arr = input.split(' ')
+  arr = []
+  result = []
+  input.split(' ').map do |word|
+    arr = word.split('')
+    first_letter = arr.shift
+    last_letter = arr.pop
+    if arr.length > 1
+      result << (arr.shuffle.unshift(first_letter).push(last_letter).join)
+    else
+      result << arr.unshift(first_letter).push(last_letter).join
+    end
+  end
+  result.join(' ')
 end
