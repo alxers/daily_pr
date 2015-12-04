b74cf763324fb176bdc9c098e122bba42fca5923
Alexey Ershov <alex4rom@gmail.com>
2015-12-04 17:03:43 +0300

    Refactor

--- ch_240/typoglycemia.rb
+++ ch_240/typoglycemia.rb
@@ -1,16 +1,30 @@
-# TODO: refactor :)
-def scramble(input)
-  arr = []
-  result = []
-  input.split(' ').map do |word|
-    arr = word.split('')
-    first_letter = arr.shift
-    last_letter = arr.pop
-    if arr.length > 1
-      result << (arr.shuffle.unshift(first_letter).push(last_letter).join)
-    else
-      result << arr.unshift(first_letter).push(last_letter).join
-    end
+#TODO: fix strange behaviour (place word f.e.)
+# "Hello world place".split(' ').map { |word| arr = word.chars; f = arr.shift; l = arr.pop; p arr.shuffle.unshift(f).push(l).join  }.join(' ')
+class Scrambler
+  def initialize(input)
+    @input_arr = input.split(' ')
   end
-  result.join(' ')
+
+  def scramble
+    @input_arr.map do |word|
+      shuffle_chars(word)
+    end.join(' ')
+  end
+
+  private
+    def shuffle_chars(word)
+      chars = word.chars
+      first_letter = chars.shift
+      last_letter = chars.pop
+      chars.shuffle.unshift(first_letter).push(last_letter).join
+    end
 end
+
+scrambler = Scrambler.new("According to a research team at Cambridge University,
+                          it doesn't matter in what order the letters in a word are,
+                          the only important thing is that the first and last letter be in the right place.
+                          The rest can be a total mess and you can still read it without a problem.
+                          This is because the human mind does not read every letter by itself, but the word as a whole.
+                          Such a condition is appropriately called Typoglycemia.")
+
+p scrambler.scramble
